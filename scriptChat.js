fde2fbc052d2e6837cb52243895cf7c7cedee27c
SophieLakhotska <146372296+SophieLakhotska@users.noreply.github.com>
2023-10-20 13:17:22 +0300

    scriptChat.js

--- scriptChat.js
+++ scriptChat.js
@@ -1,28 +1,19 @@
 function sendMessage(user) {
-    var messageInput;
-    var className;
-    if (user === 'user1') {
-        messageInput = document.getElementById("user1-input");
-        className = "user1";
-    } else {
-        messageInput = document.getElementById("user2-input");
-        className = "user2";
-    }
+    const messageInput = user === 'user1' ? document.getElementById("user1-input") : document.getElementById("user2-input");
+    const className = user === 'user1' ? "user1" : "user2";
 
-    var message = messageInput.value;
-    if (message.trim() === "") {
+    const message = messageInput.value.trim();
+    if (message === "") {
         return;
     }
 
-    var chatBox = document.getElementById("chat-box");
-    var newMessage = document.createElement("div");
+    const chatBox = document.getElementById("chat-box");
+    const newMessage = document.createElement("div");
     newMessage.className = "message " + className;
     newMessage.textContent = message;
     chatBox.appendChild(newMessage);
 
-    // Очищення поля введення
     messageInput.value = "";
 
-    // Прокрутка чату до нижньої частини для відображення нового повідомлення
     chatBox.scrollTop = chatBox.scrollHeight;
 }
